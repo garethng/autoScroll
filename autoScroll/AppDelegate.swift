001d2cbeadc5df2124358deeb05092d1569e61ec
Gareth Ng <njwqz77@gmail.com>
2022-11-10 22:09:46 +0800

    Initial Commit

---
+++ autoScroll/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  autoScroll
+//
+//  Created by Gareth Ng on 2022/11/10.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
